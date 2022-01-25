9b4ecbf2a0312f455ba951dfc8359a7b1287906f
NesquikPlz <ok2006818@gmail.com>
2022-01-25 17:35:35 +0900

    code refactoring + change camera view + add comments

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from panda_env import PandaEnv
 
-def process_result(process) : # prints process, returns end condition
-    if process == 0:
+def process_result(process) : # prints process, returns process are done or not, if done moving an object, process is 4
+    if process == 0:    # process 0 : from gripper's initial state to grasp an object and pick up high
         print("--Grasped a duck--")
         return False, process + 1
-    if process == 1:
+    if process == 1:    # process 1 : moving an object from left to right(above the box)
         print("--Moving to the box--")
         return False, process + 1
-    if process == 2:
+    if process == 2:    # process 2 : move an object from above to the box (still grasping)
         print("--release the duck--")
         return False, process + 1
-    if process == 3:
+    if process == 3:    # process 3 : release a finger (put the object down) and move the panda above the box(initialize gripper's pos)
         print("--init panda's pos--")
         return False, process + 1
-    if process == 4:
+    if process == 4:    # process 4 == Done Initializing
         return True, 0
     else :
         return False, process
@@ -23,51 +23,58 @@ def main() :
     done = False
     error = 0.01
 
-    k_p = 3
+    k_p = 3         # set panda's base speed
     k_d = 1
     dt = 1. / 240.  # the default timestep in pybullet is 240 Hz
 
-    observation = env.reset()
+    panda_position = env.reset()
 
-    for i in range(1, 4) :
+    for i in range(1, 4) :  # Three objects(lego) to move.
         isEnd = False
         process = 0
-        fingers = 1
-        object_position = env.duck_position(i)
-        # env.resetJoints_panda()
-        print('object_position', object_position)
+        fingers = 1     # fingers var means the distance between two gripper's fingers.
+                        # if fingers == 1 then I want to make the gripper to release the object
+                        # if fingers == 0 then I want to make the gripper to grasp the object
+        object_position = env.duck_position(i)   # Get object's position (x, y, z)
 
-        for t in range(300):
+        for t in range(300):    #300 steps per object are available
             env.render()
-            dx = object_position[0] - observation[0]
-            dy = object_position[1] - observation[1]
-            target_z = object_position[2] - 0.007
 
-            if (observation[3] + observation[4]) < error + 0.03 and fingers == 0:
+            target_x = object_position[0]       # target x coordinate(==object x coordinate)
+            target_y = object_position[1]
+            target_z = object_position[2] - 0.006       # target z coordinate (to grasp surely, target coordinate is a bit below the target coord)
+
+            if (panda_position[3] + panda_position[4]) < error + 0.037 and fingers == 0: # if gripper grasped the object correctly
+                target_z = 0.5     # then pick the object
+            if process == 2 :   # if done moving the object above the box
+                target_z = 0    # then put it down
+            elif process == 3 :     # if done moving the object on the box
+                fingers = 1     # then release the gripper and put the object in the box
+            elif process == 4 :     # initialize panda's pos; moving above and left
                 target_z = 0.5
-            if process == 2 : # put a duck in the box
-                target_z = 0
-            elif process == 3 : # release a duck
-                fingers = 1
-            elif process == 4 : # init panda's pos
-                target_z = 0.5
-                dx = 0.3 - observation[0]
-            dz = target_z - observation[2]
+                target_x = 0.3
 
-            if abs(dx) < error and abs(dy) < error and abs(dz) < error:
-                fingers = 0     # to grasp
+            '''Calculate a distance to move'''
+            dx = target_x - panda_position[0]   # x distance to move
+            dy = target_y - panda_position[1]   # y distance to move
+            dz = target_z - panda_position[2]   # z distance to move
 
+            if abs(dx) < error and abs(dy) < error and abs(dz) < error: # if panda are ready to grasp the object
+                fingers = 0     # make the gripper grasp the object
+
+            '''Calculate a velocity'''
             pd_x = k_p * dx + k_d * dx / dt
             pd_y = k_p * dy + k_d * dy / dt
             pd_z = k_p * dz + k_d * dz / dt
-            action = [pd_x, pd_y, pd_z, fingers]
-            print("action : ", action)
-            observation, reward, done, info = env.step(action, process, i-1)
+
+            action = [pd_x, pd_y, pd_z, fingers] # this is the input of the step(is used to calculate panda's next movement)
+            # print("action : ", action)
+            panda_position, reward, done, info = env.step(action, process, i-1) # not use reward, info variables at now
             object_position = info['object_position']
 
-            if done:
-                isEnd, process = process_result(process)
-                if isEnd :
+            if done:    # if a process is done (env.step returns process is done or not)
+                isEnd, process = process_result(process)    #if process == 4 and is Done, then isEnd is True
+                if isEnd :  # isEnd is True if one object are moved into the box
                     break
                 else :
                     continue
--- panda_env.py
+++ panda_env.py
@@ -4,13 +4,13 @@ import math
 import numpy as np
 
 
-class PandaEnv():
-    def __init__(self):
+class PandaEnv(): # object name is 'duck' (not 'lego') because at first the object was three ducks(I changed because of some errors)
+    def __init__(self):     # connect to GUI(to visualize) and set the camera view(humans view)
         self.step_counter = 0
         p.connect(p.GUI)
         p.resetDebugVisualizerCamera(cameraDistance=0.7, cameraYaw=0, cameraPitch=-40, cameraTargetPosition=[0.55,-0.35,0.2])
 
-    def duck_position(self, i):
+    def duck_position(self, i):     #returns current object's position (lego1~3)
         self.duck = i - 1
         duckpos, _ = p.getBasePositionAndOrientation(self.objectUid[self.duck])
         return duckpos
@@ -19,59 +19,66 @@ class PandaEnv():
         self.duck = duck
         p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
         orientation = p.getQuaternionFromEuler([0.,-math.pi,math.pi/2.])
-        dv = 0.0022
-        dx = action[0] * dv
+
+        dv = 0.0022     # set panda's base moving distance per step
+        dx = action[0] * dv     # panda's moving distance (x coordinate)
         dy = action[1] * dv
         dz = action[2] * dv
-        fingers = action[3]
+        fingers = action[3]     # if fingers == 1 then gripper are released at this step, vice versa
 
-        currentPose = p.getLinkState(self.pandaUid, 11)
-        currentPosition = currentPose[0]
-        print('current pose : ', currentPosition)
-        newPosition = [currentPosition[0] + dx,
-                       currentPosition[1] + dy,
-                       currentPosition[2] + dz]
+        currentPose = p.getLinkState(self.pandaUid, 11)     # end-effector's current position + orientation
+        self.currentPosition = currentPose[0]               # end-effector's current position
+        # print('current pose : ', currentPosition)
+        newPosition = [self.currentPosition[0] + dx,        # new position to move at this step
+                       self.currentPosition[1] + dy,
+                       self.currentPosition[2] + dz]
         jointPoses = p.calculateInverseKinematics(self.pandaUid,11,newPosition, orientation)[0:7]
 
         p.setJointMotorControlArray(self.pandaUid, list(range(7))+[9,10], p.POSITION_CONTROL, list(jointPoses)+2*[fingers])
-
+        # move Panda's joints to newPosition
         p.stepSimulation()
         state_goal_object, _ = p.getBasePositionAndOrientation(self.objectUid[self.duck])
 
-        local_goal_object = self.objectUid[self.duck]
+        local_goal_object = self.objectUid[self.duck]   # local goal object can be a lego or the tray(at process 1 and 2)
         if process == 1 or process == 2 : # move to tray
             local_goal_object = self.trayUid
 
-        state_object, _ = p.getBasePositionAndOrientation(local_goal_object)
-        state_robot = p.getLinkState(self.pandaUid, 11)[0]
-        state_fingers = (p.getJointState(self.pandaUid,9)[0], p.getJointState(self.pandaUid, 10)[0])
+        state_object, _ = p.getBasePositionAndOrientation(local_goal_object) # the position and the orientation of the object
+        state_robot = p.getLinkState(self.pandaUid, 11)[0]      # the position of end-effector
+        state_fingers = (p.getJointState(self.pandaUid,9)[0], p.getJointState(self.pandaUid, 10)[0])    # each finger's distance to the center
 
-        print('state_object : ', state_goal_object)
-        print('process : ', process)
-        if process == 0 and state_goal_object[2]>0.4: # picked a duck successfully
-            done = True
-        elif process == 1 and state_goal_object[0]>0.65 : # moved a duck above the tray
-            done = True
-        elif process == 2 and state_goal_object[0]>0.6 and state_goal_object[2]<0.15: # release the gripper
-            done = True
-        elif process == 3 : # put a duck on the tray
-            done = True
-        elif process == 4 and currentPosition[2]>0.3 and currentPosition[0]<0.4: # initialized panda's pos
-            done = True
-        else:
-            done = False
+        done = self.processDoneCondition(process, state_goal_object, self.currentPosition)      # returns each process is done or not according to the end-effector's state and the object's state
 
         self.step_counter += 1
 
         info = {'object_position': state_object}
-        self.observation = state_robot + state_fingers
-        reward = 0
-        return np.array(self.observation).astype(np.float32), reward, done, info
+        self.panda_position = state_robot + state_fingers       #5-tuple
+        print(self.panda_position)
+        reward = 0      # not use reward var
+        return np.array(self.panda_position).astype(np.float32), reward, done, info
 
-    def reset(self):
-        duckcount = 3
+    def processDoneCondition(self, process, state_goal_object, currentPosition):    # Check if each process is done or not according to the object's state and gripper's state
+        done = False
+        if process == 0 :
+            if state_goal_object[2]>0.4: # picked a duck successfully
+                done = True
+        elif process == 1 :
+            if state_goal_object[0]>0.65 : # moved a duck above the tray
+                done = True
+        elif process == 2 : # release the gripper
+            if state_goal_object[0]>0.6 and state_goal_object[2]<0.15 :
+                done = True
+        elif process == 3 : # put a duck on the tray
+            done = True
+        elif process == 4 : # initialized panda's pos
+            if currentPosition[2]>0.3 and currentPosition[0]<0.4 :
+                done = True
+        return done
+
+    def reset(self):       # Initialize the environment
+        duckcount = 3       #3 objects to move
         rotate = [0, 0.1, 0.6, 0]
-        dis = 0.45 / duckcount
+        dis = 0.45 / duckcount  # set each object's base location
 
         self.step_counter = 0
         p.resetSimulation()
@@ -79,11 +86,15 @@ class PandaEnv():
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
 
+        '''Load Everything we need . . .'''
         self.pandaUid = p.loadURDF("franka_panda/panda.urdf", useFixedBase=True)
         self.tableUid = p.loadURDF("table/table.urdf", basePosition=[0.5, 0, -0.65])
         self.plainID = p.loadURDF("plane.urdf", basePosition=[0, 0, -0.65])
         self.trayUid = p.loadURDF("tray/traybox.urdf", basePosition=[1., 0, 0])
         self.objectUid= []
+        for i in range(duckcount):
+            inipos = [0.55 - dis * i, 0, 0]
+            self.objectUid.append(p.loadURDF("lego/lego.urdf", basePosition = inipos, baseOrientation= rotate, globalScaling = 1.5))
 
         '''reset panda's poses'''
         rest_poses = [0, -0.215, 0, -2.57, 0, 2.356, 2.356, 0.08, 0.08]
@@ -92,26 +103,42 @@ class PandaEnv():
         p.resetJointState(self.pandaUid, 9, 0.08)
         p.resetJointState(self.pandaUid, 10, 0.08)
 
-        for i in range(duckcount):
-            inipos = [0.55 - dis * i, 0, 0]
-            self.objectUid.append(p.loadURDF("lego/lego.urdf", basePosition = inipos, baseOrientation= rotate, globalScaling = 1.5))
-
-        state_robot = p.getLinkState(self.pandaUid, 11)[0]
+        '''Set class' variables'''
+        self.state_robot = p.getLinkState(self.pandaUid, 11)[0]
+        self.orien_robot = p.getLinkState(self.pandaUid, 11)[1]
+        self.currentPosition = self.state_robot
         state_fingers = (p.getJointState(self.pandaUid,9)[0], p.getJointState(self.pandaUid, 10)[0])
-        self.observation = state_robot + state_fingers
+        # print('state_fingers :', state_fingers)
+        self.panda_position = self.state_robot + state_fingers
         p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,1)
 
-        return np.array(self.observation).astype(np.float32)
+        return np.array(self.panda_position).astype(np.float32)
 
+    def render(self):       # Just camera rendering function
 
-    def render(self):
-        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.7,0,0.05],
-                                                            distance=.7,
-                                                            yaw=90,
-                                                            pitch=-70,
-                                                            roll=0,
-                                                            upAxisIndex=2)
-        proj_matrix = p.computeProjectionMatrixFOV(fov=60,
+        # view_matrix = p.computeViewMatrixFromYawPitchRoll(    #fixed camera view(view from human)
+        #                                                     cameraTargetPosition=[0.7,0,0.05],
+        #                                                     distance=.7,
+        #                                                     yaw=90,
+        #                                                     pitch=-70,
+        #                                                     roll=0,
+        #                                                     upAxisIndex=2)
+        dis = 1000.0
+        yaw = p.getEulerFromQuaternion(self.orien_robot)[-1]
+
+        xCamera = self.currentPosition[0]
+        yCamera = self.currentPosition[1]
+        zCamera = self.currentPosition[2] + 0.15
+
+        xTarget = xCamera + math.cos(yaw) * dis
+        yTarget = yCamera + math.sin(yaw) * dis
+        zTarget = zCamera + math.tan(yaw) * dis
+
+        view_matrix = p.computeViewMatrix(cameraEyePosition= [xCamera, yCamera, zCamera],
+                                          cameraTargetPosition = [xTarget,yTarget,zTarget],
+                                          cameraUpVector = [0., 0., 0.5]
+                                          )
+        proj_matrix = p.computeProjectionMatrixFOV(fov=80,
                                                      aspect=float(960) /720,
                                                      nearVal=0.1,
                                                      farVal=100.0)
@@ -128,7 +155,7 @@ class PandaEnv():
         return rgb_array
 
     def _get_state(self):
-        return self.observation
+        return self.panda_position
 
     def close(self):
         p.disconnect()
